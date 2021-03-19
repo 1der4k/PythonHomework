15b8a309e3c3bce40051f81f42acb4619d50375f
Zavala <toro.d.c@gmail.com>
2021-03-18 22:41:51 -0400

    total months in dataset in PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,24 +2,41 @@
 import os
 import csv
 
-
-# read in csv file
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
+# define function
+#def analysis(row):
+    
+
+
+
+    # The net total amount of "Profit/Losses" over the entire period; sum of values in second column
+
+    # Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes; loop through second column capaturing changes in profit/loss, sum, then divide by total number of changes; assign to new column(?)
+
+    # The greatest increase in profits (date and amount) over the entire period; find max value in changes of profit/loss column
+
+    # The greatest decrease in losses (date and amount) over the entire period; find min value in changes of profit/loss column
+
+# read in csv file
 with open(csvpath) as csvfile:
+    
     csvreader = csv.reader(csvfile, delimiter=',')
 
+    for row in csvreader:
+        date = row[0]
+        prof_loss = row[1]
     
-# analysis and print to terminal
-# The total number of months included in the dataset; sum of values in first column
+        # The total number of months included in the dataset; total number of entries in first column
+        total_months = len(list(csvreader))
+        
 
-# The net total amount of "Profit/Losses" over the entire period; sum of values in second column
+        
+    
 
-# Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes; loop through second column capaturing changes in profit/loss, sum, then divide by total number of changes; assign to new column(?)
-
-# The greatest increase in profits (date and amount) over the entire period; find max value in changes of profit/loss column
-
-# The greatest decrease in losses (date and amount) over the entire period; find min value in changes of profit/loss column
+# Run analysis
 
 
-# export text file with results
+# Print to terminal
+
+# Export to file
