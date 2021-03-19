fc90eb43efbeb8b99c497719e31df54a203e2598
Zavala <toro.d.c@gmail.com>
2021-03-19 19:43:11 -0400

    trying to solve error: unsupported operand type(s) for +: 'int' and 'str'

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,28 +7,23 @@ csvpath = os.path.join('Resources', 'election_data.csv')
 # define function
 def votes_analysis(row):
     votes = []
-    vote = row[0]
+
     cand_list = []
-    cand_vote = row[2]
 
     khan_vote = []
     correy_vote = []
     li_vote = []
     otooley_vote = []
-
-    khan_percent = sum(khan_vote)/len(khan_vote) * 100
-    correy_percent = sum(correy_vote)/len(correy_vote) * 100
-    li_percent = sum(li_vote)/len(li_vote) * 100
-    otooley_percent = sum(otooley_vote)/len(otooley_vote) * 100
-
-    cand_vote_totals = [sum(khan_vote,sum(correy_vote,sum(li_vote,sum(otooley_vote))))]
-    winner = max(cand_vote_totals)
-    
+      
     # The total number of votes cast
+    vote = row[0]
     votes.append(vote)
-    # A complete list of candidates who received votes   
+
+    # A complete list of candidates who received votes  
+    cand_vote = row[2] 
     if cand_vote not in cand_list:
             cand_list.append(cand_vote)
+
     # The total number of votes each candidate won  
     if cand_vote == "Khan":
         khan_vote.append(cand_vote)
@@ -38,19 +33,26 @@ def votes_analysis(row):
         li_vote.append(cand_vote)
     elif cand_vote == "o'Tooley":
         otooley_vote.append(cand_vote)
+
     # The percentage of votes each candidate won
+    khan_percent = int(sum(khan_vote))/int(len(khan_vote)) * 100
+    correy_percent = int(sum(correy_vote))/int(len(correy_vote)) * 100
+    li_percent = int(sum(li_vote))/int(len(li_vote)) * 100
+    otooley_percent = int(sum(otooley_vote))/int(len(otooley_vote)) * 100
 
     # The winner of the election based on popular vote.
+    cand_vote_totals = [sum(khan_vote,sum(correy_vote,sum(li_vote,sum(otooley_vote))))]
+    winner = max(cand_vote_totals)
 
     # Print to terminal
     print("Election Results")
     print("-------------------------------------------")
     print("Total Votes: " + str(len(votes)))
     print("-------------------------------------------")
-    print("Khan: " + round(khan_percent, 3)) + " (" + votes.count("Khan") +")"
-    print("Correy: " + round(correy_percent, 3)) + " (" + votes.count("Correy") +")"
-    print("Li: " + round(li_percent, 3)) + " (" + votes.count("Li") +")"
-    print("O'Tooley: " + round(otooley_percent, 3)) + " (" + votes.count("O'Tooley") +")"
+    print("Khan: " + str(round(khan_percent, 3)) + " (" + vstr(otes.count("Khan")) +")")
+    print("Correy: " + str(round(correy_percent, 3)) + " (" + str(votes.count("Correy")) +")")
+    print("Li: " + str(round(li_percent, 3)) + " (" + str(votes.count("Li")) +")")
+    print("O'Tooley: " + str(round(otooley_percent, 3)) + " (" + str(votes.count("O'Tooley")) +")")
     print("-------------------------------------------")
     print("Winner: " + winner)
 
@@ -63,7 +65,7 @@ with open(csvpath, 'r') as csvfile:
 
 # Analysis
     for row in csvreader:
-        votes_analysis
+        votes_analysis(row)
 
        
 
