7e086f88a673278ba9dbfb5ceb79bc1e20627f53
Zavala <toro.d.c@gmail.com>
2021-03-20 18:02:43 -0400

    automated capture and print of candidate names, problem with printing vote percent and vote count

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,10 +10,9 @@ def votes_analysis(data):
 
     cand_list = []
 
-    khan_vote = []
-    correy_vote = []
-    li_vote = []
-    otooley_vote = []
+    cand_votes = []
+
+    vote_percent = []
 
     for row in data:  
     # The total number of votes cast
@@ -25,37 +24,43 @@ def votes_analysis(data):
         if cand_vote not in cand_list:
             cand_list.append(cand_vote)
 
-        # The total number of votes each candidate won  
-        if cand_vote == "Khan":
-            khan_vote.append(cand_vote)
-        elif cand_vote == "Correy":
-            correy_vote.append(cand_vote)
-        elif cand_vote == "Li":
-            li_vote.append(cand_vote)
-        elif cand_vote == "O'Tooley":
-            otooley_vote.append(cand_vote)
+    # The total number of votes each candidate won
+    for candidate in cand_list:
+        cand_votes.append(votes.count(candidate))   
+    # if cand_vote == "Khan":
+    #     khan_vote.append(cand_vote)
+    # elif cand_vote == "Correy":
+    #     correy_vote.append(cand_vote)
+    # elif cand_vote == "Li":
+    #     li_vote.append(cand_vote)
+    # elif cand_vote == "O'Tooley":
+    #     otooley_vote.append(cand_vote)
 
-        # The percentage of votes each candidate won
-        khan_percent = int(len(khan_vote))/len(votes) * 100
-        correy_percent = int(len(correy_vote))/len(votes) * 100
-        li_percent = int(len(li_vote))/len(votes)  * 100
-        otooley_percent = int(len(otooley_vote))/len(votes)  * 100
+    # The percentage of votes each candidate won
+        vote_percent.append(votes.count(candidate)/int(len(votes)) * 100)
+    # khan_percent = int(len(khan_vote))/len(votes) * 100
+    # correy_percent = int(len(correy_vote))/len(votes) * 100
+    # li_percent = int(len(li_vote))/len(votes)  * 100
+    # otooley_percent = int(len(otooley_vote))/len(votes)  * 100
 
-        # The winner of the election based on popular vote.
-        cand_vote_totals = [int(len(khan_vote)),int(len(correy_vote)),int(len(li_vote)),int(len(otooley_vote))]
-        winner = cand_list[cand_vote_totals.index(max(cand_vote_totals))]
+    # The winner of the election based on popular vote.
+    winner = cand_list[cand_votes.index(max(cand_votes))]
+    # cand_vote_totals = [int(len(khan_vote)),int(len(correy_vote)),int(len(li_vote)),int(len(otooley_vote))]
+    # winner = cand_list[cand_vote_totals.index(max(cand_vote_totals))]
 
     # Print to terminal
     print("Election Results")
     print("-------------------------------------------")
     print("Total Votes: " + str(len(votes)))
     print("-------------------------------------------")
-    print("Khan: " + str(round(khan_percent,3)) + "%" + " (" + str(khan_vote.count("Khan")) +")")
-    print("Correy: " + str(round(correy_percent,3)) + "%"  + " (" + str(correy_vote.count("Correy")) +")")
-    print("Li: " + str(round(li_percent,3)) + "%"  + " (" + str(li_vote.count("Li")) +")")
-    print("O'Tooley: " + str(round(otooley_percent,3)) + "%"  + " (" + str(otooley_vote.count("O'Tooley")) +")")
+    for i in range(len(cand_list)):
+        print(f'{cand_list[i]}: {round(vote_percent[i])}% {cand_votes[i]}')
+    # print("Khan: " + str(round(khan_percent,3)) + "%" + " (" + str(khan_vote.count("Khan")) +")")
+    # print("Correy: " + str(round(correy_percent,3)) + "%"  + " (" + str(correy_vote.count("Correy")) +")")
+    # print("Li: " + str(round(li_percent,3)) + "%"  + " (" + str(li_vote.count("Li")) +")")
+    # print("O'Tooley: " + str(round(otooley_percent,3)) + "%"  + " (" + str(otooley_vote.count("O'Tooley")) +")")
     print("-------------------------------------------")
-    print("Winner: " + str(winner))
+    print(f'Winner: {winner}')
 
 # read in csv file
 with open(csvpath, 'r') as csvfile:
