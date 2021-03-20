d1c0a88d46f8bc8a15785ecc50af86087200013c
Zavala <toro.d.c@gmail.com>
2021-03-20 18:34:37 -0400

    cleaned up PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -30,26 +30,12 @@ def election_analysis(data):
     # The total number of votes each candidate won
     for candidate in cand_list:
         cand_votes.append(votes.count(candidate))   
-    # if cand_vote == "Khan":
-    #     khan_vote.append(cand_vote)
-    # elif cand_vote == "Correy":
-    #     correy_vote.append(cand_vote)
-    # elif cand_vote == "Li":
-    #     li_vote.append(cand_vote)
-    # elif cand_vote == "O'Tooley":
-    #     otooley_vote.append(cand_vote)
-
+    
     # The percentage of votes each candidate won
         vote_percent.append(votes.count(candidate)/int(len(votes)) * 100)
-    # khan_percent = int(len(khan_vote))/len(votes) * 100
-    # correy_percent = int(len(correy_vote))/len(votes) * 100
-    # li_percent = int(len(li_vote))/len(votes)  * 100
-    # otooley_percent = int(len(otooley_vote))/len(votes)  * 100
 
     # The winner of the election based on popular vote.
     winner = cand_list[cand_votes.index(max(cand_votes))]
-    # cand_vote_totals = [int(len(khan_vote)),int(len(correy_vote)),int(len(li_vote)),int(len(otooley_vote))]
-    # winner = cand_list[cand_vote_totals.index(max(cand_vote_totals))]
 
     # Print to terminal
     print("Election Results")
@@ -58,10 +44,6 @@ def election_analysis(data):
     print("-------------------------------------------")
     for i in range(len(cand_list)):
         print(f'{cand_list[i]}: {round(vote_percent[i],3)}% ({cand_votes[i]})')
-    # print("Khan: " + str(round(khan_percent,3)) + "%" + " (" + str(khan_vote.count("Khan")) +")")
-    # print("Correy: " + str(round(correy_percent,3)) + "%"  + " (" + str(correy_vote.count("Correy")) +")")
-    # print("Li: " + str(round(li_percent,3)) + "%"  + " (" + str(li_vote.count("Li")) +")")
-    # print("O'Tooley: " + str(round(otooley_percent,3)) + "%"  + " (" + str(otooley_vote.count("O'Tooley")) +")")
     print("-------------------------------------------")
     print(f'Winner: {winner}')
     print("-------------------------------------------")
@@ -89,25 +71,3 @@ with open(csvpath, 'r') as csvfile:
 
 # Analysis
     election_analysis(csvreader)
-
-
-
-       
-
-
-        
-        
- 
-
-        
-
-
-
-# Print to terminal
-#print("Election Results")
-#print("-------------------------------------------")
-#print("Total Votes: " + str(len(votes)))
-#print("-------------------------------------------")
-#print(cand_list[:])
-
-# Export to text file
